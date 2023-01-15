3476fc09604856596194151b330bc86a8534ce4b
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-15 22:57:34 +0100

    Aggiunto footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,74 @@
+<footer>
+    <section class="footer-container">
+
+        <div class="container-fluid py-4">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-4 d-flex">
+                    <div class="col d-flex flex-column">
+                        <h6>DC COMICS</h6>
+                        <small>Characters</small>
+                        <small>comics</small>
+                        <small>Movies</small>
+                        <small>TV</small>
+                        <small>Games</small>
+                        <small>Videos</small>
+                        <small>News</small>
+                        <h6 class="pt-2">SHOP</h6>
+                        <small>Shop DC</small>
+                        <small>Shop DC collectibles</small>
+                    </div>
+                    <div class="col d-flex flex-column">
+                        <h6>DC</h6>
+                        <small>Terms of Use</small>
+                        <small>Privacy Policy (New)</small>
+                        <small>Ad Choices</small>
+                        <small>Advertising</small>
+                        <small>Jobs</small>
+                        <small>Subscriptions</small>
+                        <small>Talent Workshops</small>
+                        <small>CPSC certificates</small>
+                        <small>Ratings</small>
+                        <small>Shop Help</small>
+                    </div>
+                    <div class="col d-flex flex-column">
+                        <h6>SITES</h6>
+                        <small>DC</small>
+                        <small>MAD Magazines</small>
+                        <small>DC Kids</small>
+                        <small>DC Universe</small>
+                        <small>DC Power Visa</small>
+                    </div>
+                </div>
+                <div class="col-4 text-center">
+                    <img class="footer-img" src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="footer-bottom py-4">
+
+        <div class="container-fluid">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-4">
+                    <button class="btn">SING-UP NOW</button>
+                </div>
+                <div class="col-4 d-flex justify-content-center align-items-center">
+                    <h6 class="follow">FOLLOW US</h6>
+                    <img class="social px-1" src="{{ Vite::asset('resources/img/footer-facebook.png') }}"
+                        alt="">
+                    <img class="social px-1" src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                    <img class="social px-1" src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                    <img class="social px-1" src="{{ Vite::asset('resources/img/footer-pinterest.png') }}"
+                        alt="">
+                    <img class="social px-1" src="{{ Vite::asset('resources/img/footer-periscope.png') }}"
+                        alt="">
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+</footer>
